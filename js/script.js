1036e74a6230ceb31259651255af105cb53d66e1
Veronica0014 <119103207+Veronica0014@users.noreply.github.com>
2024-02-02 14:20:20 +0100

    small changes

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
-// BURGER ICON
+// BURGER MENU ICON
 
-const mobileBtn = document.getElementById("mobile-cta");
-      const nav = document.querySelector("nav");
-      const mobileBtnExit = document.getElementById("mobile-exit");
+const mobileBtn = document.getElementById('mobile-cta')
+const nav = document.querySelector('nav')
+const mobileBtnExit = document.getElementById('mobile-exit')
 
-      // When Menu is clicked
-      mobileBtn.addEventListener("click", () => {
-        nav.classList.add("menu-btn");
-      });
+// When menu is clicked
+mobileBtn.addEventListener('click', () => {
+	nav.classList.add('menu-btn')
+})
 
-      // When Exit is clicked
-      mobileBtnExit.addEventListener("click", () => {
-        nav.classList.remove("menu-btn");
-      });
-
-
+// When exit is clicked
+mobileBtnExit.addEventListener('click', () => {
+	nav.classList.remove('menu-btn')
+})
 
 // have a nice day par
 
+const now = new Date()
+
+const daysOfWeek = ['niedziela', 'poniedziałek', 'wtorek', 'środa', 'Czwartek', 'piątku', 'sobota']
+const dayOfWeek = daysOfWeek[now.getDay()]
+
+document.querySelector('.hello-change').innerHTML = `${dayOfWeek}`
